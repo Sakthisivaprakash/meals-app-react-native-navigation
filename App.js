be585e24777bc5fcefd9deca7632d86993b0018a
Sakthisivaprakash <sakthisivaprakash@gmail.com>
2022-09-18 23:34:18 +0530

    styling individual screen and its header from navigator stack

--- App.js
+++ App.js
@@ -14,9 +14,18 @@ export default function App() {
       <StatusBar style="dark" />
       <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
-            <Stack.Screen name="MealsOverview" component={MealsOverviewScreen}/>
-        </Stack.Navigator>      
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: 'All Categories',
+              headerStyle: {backgroundColor: '#351401'},
+              headerTintColor: 'white',
+              contentStyle: {backgroundColor:'#3f2f25'}
+            }}
+          />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
